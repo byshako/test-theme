c7fe5fc4de648a65f9ab12dca68c2a327ffd6086
byshako1 <sh.kavtaradze@adjarabet.com>
2018-04-24 12:26:42 +0400

    Navigation

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    // Menu script
+    $("#menu").click(function() {
+        $(".main-page").toggleClass("active");
+        $(".nav").toggleClass("active");
+    });
+});
