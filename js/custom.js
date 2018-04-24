fb1670ea0f1922a1e6ad47f983ddd14eddaf4e34
byshako1 <sh.kavtaradze@adjarabet.com>
2018-04-24 15:26:11 +0400

    Some changes

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,21 @@
 $(document).ready(function() {
     // Menu script
     $("#menu").click(function() {
-        $(".main-page").toggleClass("active");
-        $(".main-page__perspective").toggleClass("active");
-        $(".nav").toggleClass("active");
+        $(".toggle").toggleClass("active");
         $(".fas.fa-bars").toggleClass("fa-times");
     });
+    // Menu Click
+    $(".navigation__item-link").click(function() {
+        $(".toggle").toggleClass("active");
+        $(".fas.fa-bars").toggleClass("fa-times");
+    });
+    $(document).on('click', 'a[href^="#"]', function(e) {
+        var id = $(this).attr('href');
+        var $id = $(id);
+        if ($id.length === 0){return;}
+        e.preventDefault();
+        var pos = $id.offset().top - 60;
+        $('body, html').animate({scrollTop: pos});
+    });
+    // Header bg
 });
