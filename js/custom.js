ebbb7a5c5a00d8c8711eec5aed9624b848124367
byshako1 <sh.kavtaradze@adjarabet.com>
2018-04-24 14:09:07 +0400

    Side Menu function & styles

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
     // Menu script
     $("#menu").click(function() {
         $(".main-page").toggleClass("active");
+        $(".main-page__perspective").toggleClass("active");
         $(".nav").toggleClass("active");
+        $(".fas.fa-bars").toggleClass("fa-times");
     });
 });
