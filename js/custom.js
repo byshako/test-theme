b38c5da23f585d745b6302a317a45632400d9053
shalva kavtaradze <byshako@gmail.com>
2018-04-26 11:20:32 +0400

    DELETE TEXT EFFECTS

--- js/custom.js
+++ js/custom.js
@@ -19,13 +19,5 @@ $(document).ready(function() {
     });
 
     // Text Animation
-    $(function() {
-        var list = ['Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut debitis expedita fugiat, fugit minima quisquam.'
-            ,'necessitatibus neque nihil odio officia provident quaerat quam quia quidem quoratione repellat saepe ut vel voluptate!'
-            , 'dolores expedita harum magnam molestiae natus quia voluptas? Aperiam distinctiodoloremque molestias praesentium quis? Amet, sapiente.'
-            , 'Accusantium alias aliquam aliquid debitis distinctio, ducimus earum eum fuga sunt. Inventoreratione repellendus voluptatem.'];
-        var txt = $('#txtlzr');
-        txt.textualizer(list, { duration: 3000, centered:true });
-        txt.textualizer('start');
-    })
+
 });
